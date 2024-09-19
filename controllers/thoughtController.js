4643a62d66398a316d3d56499e17abdb91adfac9
erkanoktenn <erkannoktenn@gmail.com>
2024-09-18 23:13:42 -0400

    Improve error handling and validation in updateThought function

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -37,18 +37,32 @@ module.exports = {
   },
 
   // Update thought
-  async updateThought(req, res) {
-    try {
-      const thought = await Thought.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true });
-      if (!thought) {
-        return res.status(404).json({ message: 'No thought found with this id!' });
-      }
-      res.status(200).json(thought);
-    } catch (err) {
-      res.status(400).json(err);
-    }
-  },
+async updateThought(req, res) {
+  try {
+    const { thoughtId } = req.params;
+    const updateData = req.body;
 
+    // Ensure there is something to update
+    if (!Object.keys(updateData).length) {
+      return res.status(400).json({ message: 'No update data provided' });
+    }
+
+    const thought = await Thought.findOneAndUpdate(
+      { _id: thoughtId },
+      { $set: updateData },
+      { runValidators: true, new: true }
+    );
+
+    if (!thought) {
+      return res.status(404).json({ message: 'No thought found with that ID' });
+    }
+
+    res.json(thought);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: 'Internal server error' });
+  }
+},
   // Delete thought
   async deleteThought(req, res) {
     try {
--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -11,7 +11,7 @@ const {
 
 router.route('/').get(getAllThoughts).post(createThought);
 
-router.route('/:id').get(getThoughtById).put(updateThought).delete(deleteThought);
+router.route('/:thoughtId').get(getThoughtById).put(updateThought).delete(deleteThought);
 
 router.route('/:thoughtId/reactions').post(addReaction);
 
